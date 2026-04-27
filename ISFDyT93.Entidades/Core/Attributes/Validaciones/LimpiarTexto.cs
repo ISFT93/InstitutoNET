480361e47a6e7c469e9dda0964f61eebbc749031
Franco Daniel Fernandez <133538118+FrancoDF7@users.noreply.github.com>
2026-04-27 12:09:46 -0300

    feature: Nueva clase con validacion en capa Entidades

---
+++ ISFDyT93.Entidades/Core/Attributes/Validaciones/LimpiarTexto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISFDyT93.Datos.Core.Attributes.Validaciones
+{
+    public static class LimpiarTexto
+    {
+        //Elimina saltos de linea invisibles cuando se valida el texto,
+        //y espacios al principio y final del texto.
+        public static string QuitarSaltosDeLineaYEspacios(string texto)
+        {
+            return texto
+                .Replace("\r", "")
+                .Replace("\n", "")
+                .Trim();
+        }
+
+    }
+}
