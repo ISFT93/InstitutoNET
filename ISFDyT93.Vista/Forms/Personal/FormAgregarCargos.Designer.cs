587469e66f425a762c676128c6474d4c649b9700
NiFaNa <segundofacu@gmail.com>
2026-05-10 19:58:26 -0300

    Cambios hechos al apartado de parámetros para los cargos

---
+++ ISFDyT93.Vista/Forms/Personal/FormAgregarCargos.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace ISFDyT93.Vista.Forms.Personal
+{
+    partial class FormAgregarCargos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbTipoAsignacion = new System.Windows.Forms.ComboBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCargaHoraria = new System.Windows.Forms.TextBox();
+            this.cmbTipoAplicacion = new System.Windows.Forms.ComboBox();
+            this.panelTitulo = new System.Windows.Forms.Panel();
+            this.lblCargos = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAñadir = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.panelTitulo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmbTipoAsignacion
+            // 
+            this.cmbTipoAsignacion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTipoAsignacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbTipoAsignacion.FormattingEnabled = true;
+            this.cmbTipoAsignacion.Location = new System.Drawing.Point(46, 145);
+            this.cmbTipoAsignacion.Name = "cmbTipoAsignacion";
+            this.cmbTipoAsignacion.Size = new System.Drawing.Size(165, 28);
+            this.cmbTipoAsignacion.TabIndex = 0;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(46, 86);
+            this.txtNombre.MaxLength = 50;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.ShortcutsEnabled = false;
+            this.txtNombre.Size = new System.Drawing.Size(165, 26);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
+            // 
+            // txtCargaHoraria
+            // 
+            this.txtCargaHoraria.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCargaHoraria.Location = new System.Drawing.Point(302, 86);
+            this.txtCargaHoraria.MaxLength = 2;
+            this.txtCargaHoraria.Name = "txtCargaHoraria";
+            this.txtCargaHoraria.ShortcutsEnabled = false;
+            this.txtCargaHoraria.Size = new System.Drawing.Size(165, 26);
+            this.txtCargaHoraria.TabIndex = 2;
+            this.txtCargaHoraria.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCargaHoraria_KeyPress);
+            // 
+            // cmbTipoAplicacion
+            // 
+            this.cmbTipoAplicacion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTipoAplicacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbTipoAplicacion.FormattingEnabled = true;
+            this.cmbTipoAplicacion.Location = new System.Drawing.Point(302, 145);
+            this.cmbTipoAplicacion.Name = "cmbTipoAplicacion";
+            this.cmbTipoAplicacion.Size = new System.Drawing.Size(165, 28);
+            this.cmbTipoAplicacion.TabIndex = 3;
+            // 
+            // panelTitulo
+            // 
+            this.panelTitulo.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.panelTitulo.Controls.Add(this.lblCargos);
+            this.panelTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitulo.Location = new System.Drawing.Point(0, 0);
+            this.panelTitulo.Name = "panelTitulo";
+            this.panelTitulo.Size = new System.Drawing.Size(523, 37);
+            this.panelTitulo.TabIndex = 4;
+            // 
+            // lblCargos
+            // 
+            this.lblCargos.AutoSize = true;
+            this.lblCargos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCargos.ForeColor = System.Drawing.Color.White;
+            this.lblCargos.Location = new System.Drawing.Point(167, 9);
+            this.lblCargos.Name = "lblCargos";
+            this.lblCargos.Size = new System.Drawing.Size(164, 20);
+            this.lblCargos.TabIndex = 0;
+            this.lblCargos.Text = "Añadir nuevo cargo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nombre del cargo";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(299, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Carga horaria";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tipo de asignación";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(299, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tipo de aplicación";
+            // 
+            // btnAñadir
+            // 
+            this.btnAñadir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnAñadir.FlatAppearance.BorderSize = 0;
+            this.btnAñadir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAñadir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAñadir.ForeColor = System.Drawing.Color.White;
+            this.btnAñadir.Location = new System.Drawing.Point(46, 202);
+            this.btnAñadir.Name = "btnAñadir";
+            this.btnAñadir.Size = new System.Drawing.Size(165, 38);
+            this.btnAñadir.TabIndex = 9;
+            this.btnAñadir.Text = "Añadir";
+            this.btnAñadir.UseVisualStyleBackColor = false;
+            this.btnAñadir.Click += new System.EventHandler(this.btnAñadir_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.Red;
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.White;
+            this.btnCancelar.Location = new System.Drawing.Point(302, 202);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(165, 38);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // FormAgregarCargos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 266);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAñadir);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelTitulo);
+            this.Controls.Add(this.cmbTipoAplicacion);
+            this.Controls.Add(this.txtCargaHoraria);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.cmbTipoAsignacion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormAgregarCargos";
+            this.Text = "FormAgregarCargos";
+            this.Load += new System.EventHandler(this.FormAgregarCargos_Load);
+            this.panelTitulo.ResumeLayout(false);
+            this.panelTitulo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbTipoAsignacion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCargaHoraria;
+        private System.Windows.Forms.ComboBox cmbTipoAplicacion;
+        private System.Windows.Forms.Panel panelTitulo;
+        private System.Windows.Forms.Label lblCargos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAñadir;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
