dbf955916ebe4015ab4d84e211c8f884a1434085
NiFaNa <segundofacu@gmail.com>
2026-05-13 19:46:51 -0300

    Update QueryRequerimiento9.sql
    Sentencias necesarias para el funcionamiento del requerimiento 9

--- Base de Datos/QueryRequerimiento9.sql
+++ Base de Datos/QueryRequerimiento9.sql
@@ -1,17 +1,29 @@
-select * from tipoaplicacion
+USE Instituto_db
+GO
 
-delete from TipoAplicacion
+ALTER TABLE Cargos ADD CargaHoraria INT
+GO
+
+CREATE PROCEDURE InsertarCargo
+    @Descripcion VARCHAR(50),
+    @CargaHoraria INT,
+    @TipoAsignacionId INT,
+    @TipoAplicacionId INT
+AS
+BEGIN
+    INSERT INTO Cargos
+    (Descripcion, CargaHoraria, TipoAsignacionId, TipoAplicacionId, Activo)
+    VALUES
+    (@Descripcion, @CargaHoraria, @TipoAsignacionId, @TipoAplicacionId, 1)
+END
+GO
+
+DELETE FROM TipoAplicacion
+GO
 
 INSERT INTO TipoAplicacion (Descripcion, Detalle) 
 VALUES 
 ('= 1', 'El personal toma el cargo y no puede ser tomado x otro ni tomar otro cargo'),
 ('> 1', 'Puede asignarse, ser asignado por otros y a la vez asignarse a otros cargos'),
 ('>= 1', 'Una vez asignado, el personal no puede signarse otro cargo pero se puede asignar el cargo a otro personal.')
-
-select * from cargos
-
-select c.cargoid, c.Descripcion, c.Activo, ta.Descripcion from Cargos c inner join TipoAplicacion ta on ta.TipoAplicacionId = c.TipoAplicacionId
-
-update TipoAplicacion set Descripcion = '>= 1' Where TipoAplicacionId = 2 
-
-update cargos set TipoAplicacionId = 3 where cargoid = 4025
+GO
